383dea483a640e00c991b62ccdee9b65f6669cb6
rosenclever <rosenclever@gmail.com>
2023-05-15 21:06:19 -0300

    crud do blog funcionando

--- Relacionamentos/Models/Blog.cs
+++ Relacionamentos/Models/Blog.cs
@@ -2,11 +2,11 @@
 {
     public class Blog
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public virtual Uri SiteUri { get; set; }
 
-        public ICollection<Post> Posts { get; }
+        public ICollection<Post>? Posts { get; }
 
     }
 }
